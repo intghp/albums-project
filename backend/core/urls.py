61dfc3d24d24020047e87c65fd503026a2be7cf4
intghp <gustavohlucenapaiva1@gmail.com>
2025-09-29 21:56:25 -0300

    ✨ feat: add core urls.py

---
+++ backend/core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from albums.views import RegisterView
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('albums.urls')),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
